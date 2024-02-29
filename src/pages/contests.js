e85b51418c55b9d50d9e69033c5fc9c85a1d4464
Christopher Dancy <chris.dancy@decentlabs.io>
2024-02-29 10:07:04 -0500

    init contract interaction

--- src/pages/contests.js
+++ src/pages/contests.js
@@ -1,9 +1,10 @@
 import UserContests from "../components/MyContests/UserContests";
 import ContestPicker from "../components/ContestPicker/ContestPicker";
-import { useReadContract } from "wagmi"; // Import useReadContract hook
-import { abi } from "../components/data/abi"; // Import the ABI of your smart contract
+import {  useWriteContract } from "wagmi"; // Import useReadContract hook
+import { abi } from "../components/data/fantasyFactory.json"; // Import the ABI of your smart contract
 
 const ContestsPage = () => {
+  const { data: hash, writeContract } = useWriteContract() 
   const mockContest = {
     contestName: "Mock Contest",
     points: 100,
@@ -15,17 +16,6 @@ const ContestsPage = () => {
 
   const mockData = [mockContest];
 
-  const {
-    data: activeContests,
-    isLoading,
-    error,
-  } = useReadContract({
-    address: "YOUR_CONTRACT_ADDRESS",
-    abi: abi,
-    functionName: "getActiveContests", // Replace 'getActiveContests' with the actual function name in your smart contract
-    args: [], // If the function takes arguments, pass them here
-  });
-
   const activeContestColumns = [
     { Header: "Contest", accessor: "contestName" },
     { Header: "Points", accessor: "points" },
@@ -35,30 +25,46 @@ const ContestsPage = () => {
     { Header: "", accessor: "actions" },
   ];
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+  // if (isLoading) {
+  //   return <p>Loading...</p>;
+  // }
 
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
+  // if (error) {
+  //   return <p>Error: {error.message}</p>;
+  // }
+  let activeContests;
+  const currentTime = new Date(); // Get the current time
+  const twoMinutesLater = new Date(currentTime.getTime() + 2 * 60000)
 
   return (
     <>
       <div>
-        {activeContests && activeContests.length > 0 ? (
-          <UserContests
-            label="Your Contests"
-            columns={activeContestColumns}
-            data={activeContests}
-          />
-        ) : (
           <UserContests
             label="Your Contests"
             columns={activeContestColumns}
             data={[]} // Empty array if no active contests are found
           />
-        )}
+         <button
+          type="submit"
+          className="rounded-md bg-lime-700 px-3 py-2 text-sm font-semibold shadow-sm hover:bg-green-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          onClick={
+            writeContract({
+              address: "0xEc2638E834848717bd991BC2c5FBDd9C19EEf5Be",
+              abi: abi,
+              functionName: "createGame", 
+              args: [
+                0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d, // vault
+                twoMinutesLater, // _gameStart
+                1, // _nDays
+                1, //_gameCost 
+                10, // _maxEntries
+                true, // _noLose
+              ],
+            })
+          }
+        >
+          Enter Contest
+        </button>
       </div>
       <div>
         <div class="container mx-auto pt-10">
